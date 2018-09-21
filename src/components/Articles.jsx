a7e4cc71f696da62a623dcb98027dd1015ee3873
andy hunt <andyhunt200@hotmail.com>
2018-09-21 12:10:05 +0100

    Basic grid added

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -7,7 +7,8 @@ import sortBy from 'lodash.sortby';
 
 class Articles extends Component {
     state = {
-        articles: []
+        articles: [],
+        page: 0
     }
     render() {
         const articles = this.state.articles;
@@ -20,30 +21,33 @@ class Articles extends Component {
         //     console.log(article)
         // })
         return (
-            <div className="articles">
-                <button className="sorting" onClick={() => this.dateSortArticles('newest')}>Newest</button>
-                <button className="sorting" onClick={() => this.dateSortArticles('oldest')}>Oldest</button>
-                <button className="sorting" onClick={() => this.trendSortArticles()}>Trending</button>
-                <ul>
-                    {articles.map(article => {
-                        const articleRating = (+article.votes + +article.comment_count);
-                        const articleAge = article.created_at;
-                        // console.log(articleAge);
-                        // console.log(articleRating);
+            <div className="articles grid-container">
+                <div className="article-sort">
+                    <button className="sorting" onClick={() => this.dateSortArticles('newest')}>Newest</button>
+                    <button className="sorting" onClick={() => this.dateSortArticles('oldest')}>Oldest</button>
+                    <button className="sorting" onClick={() => this.trendSortArticles()}>Trending</button>
+                </div>
+                {articles.slice(this.state.page, 8).map((article, i) => {
+                    const articleRating = (+article.votes + +article.comment_count);
+                    const articleAge = article.created_at;
+                    // console.log(articleAge);
+                    // console.log(articleRating);
 
-                        const createdAt = moment(article.created_at).format('MMMM Do YYYY, h:mm:ss a');
-                        return (
-                            <div className="article" key={article._id}>
-                                <div className="voter"><button><i className="far fa-arrow-alt-circle-up"></i></button><p>{article.votes}</p>
-                                    <button><i className="far fa-arrow-alt-circle-down"></i></button></div>
-                                <Link key={article._id} to={`/articles/${article._id}`} > <article key={article._id}>{article.title}</article></Link>
-                                <p>Topic: {article.belongs_to}</p>
-                                <p>Comments: {article.comment_count}</p>
-                                <p>Posted by: {article.created_by.username} {'(' + createdAt + ')'}</p>
-                            </div>)
-                    }
-                    )}
-                </ul>
+                    const createdAt = moment(article.created_at).format('MMMM Do YYYY, h:mm:ss a');
+                    return (
+                        <div className={`article area${i}`} key={article._id}>
+                            <div className="voter"><button><i className="far fa-arrow-alt-circle-up"></i></button><p>{article.votes}</p>
+                                <button><i className="far fa-arrow-alt-circle-down"></i></button></div>
+                            <Link key={article._id} to={`/articles/${article._id}`} > <article key={article._id}>{article.title}</article></Link>
+                            <p>Topic: {article.belongs_to}</p>
+                            <p>Comments: {article.comment_count}</p>
+                            <p>Posted by: {article.created_by.username} {'(' + createdAt + ')'}</p>
+                        </div>)
+                }
+                )}
+                <div class="pagination">
+                    <p>Pagination</p>
+                </div>
             </div>
         );
     }
